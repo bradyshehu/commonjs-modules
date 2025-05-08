2963b6a224b859fb6c7f6da32f80cefbb973f2e8
bradyshehu <shehu.brady@gmail.com>
2025-05-08 18:23:30 +0200

    finish commonjs people

--- main.js
+++
@@ -1,9 +0,0 @@
-// import name from "./names.js";
-
-const name = require("./names.js");
-
-console.log(name("Brady", "Shehu"));
-
-const hobbies = require("./hobbies.js");
-
-console.log(hobbies("musica", "leggere", "ballare"));
